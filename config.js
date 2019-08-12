0e6da257b790b0211399965e1ea8484c1958326b
Nathan Glick <nxg9997@rit.edu>
2019-08-12 15:37:55 -0400

    mmmmmm

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 
 var config = {
-    steamKey: "8027B0DDF4E2BA135F97EAB97E5FC425",
-    password: "9433",
-    ballchasingKey: "lInNl4ngFh5IJvGkU6Fe0r3GdUQEIWAsaJAQ1dQf",
-    mysql_user: 'bm4pfglykrzd2h1l',
-    mysql_pass: 'pehaea067d3463ev',
-    mysql_db: 'z29s1f1fqpl9mk20',
-    mysql_host: 'y06qcehxdtkegbeb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+    steamKey: "",
+    password: "",
+    ballchasingKey: "",
+    mysql_user: '',
+    mysql_pass: '',
+    mysql_db: '',
+    mysql_host: ''
 }
 
 module.exports.getConfig = function(){
